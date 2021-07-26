255d2b2a8fc391a729dcde36211fce84562269ac
manishktx <manishkumarj357@gmail.com>
2021-07-26 10:23:23 +0530

    Petty profile flow

---
+++ app/src/main/java/com/constructionmitra/user/ui/dialogs/GetFirmDetailsDialog.kt
@@ -0,0 +1,72 @@
+package com.constructionmitra.user.ui.dialogs
+
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import com.constructionmitra.user.databinding.DialogAppAlertBinding
+
+class GetFirmDetailsDialog(val onClick:() -> Unit) : DialogFragment() {
+
+    lateinit var binding: DialogAppAlertBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.let {
+            it.setGravity(Gravity.CENTER)
+            it.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            it.setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT
+            )
+            it.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DialogAppAlertBinding.inflate(inflater, container, false).apply {
+
+        }
+        context ?: return binding.root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivYes.setOnClickListener {
+            dismiss()
+            onClick()
+        }
+        binding.ivNo.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(onClick:() -> Unit) =
+            GetFirmDetailsDialog(onClick).apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+
+}
