9bf4d003439e5da6bcb3c027ae8a8a5eee4ac5f7
manishktx <manishkumarj357@gmail.com>
2021-08-10 14:36:12 +0530

    Update profile tab

--- app/src/main/java/com/constructionmitra/user/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/constructionmitra/user/ui/profile/ProfileFragment.kt
@@ -1,19 +1,18 @@
 package com.constructionmitra.user.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.constructionmitra.user.R
-import com.constructionmitra.user.databinding.FragmentAboutBinding
+import androidx.fragment.app.Fragment
+import com.constructionmitra.user.databinding.FragmentProfileBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var binding: FragmentAboutBinding
+    private lateinit var binding: FragmentProfileBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,7 +28,7 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
